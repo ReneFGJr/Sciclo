0b7ef71925dc0d9628534201fcdcc700ce63b590
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-05-05 10:36:49 -0300

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        $Seals = new \App\Models\seal\SealModel();
+        $Seals = new \App\Models\Seal\SealModel();
         $Oai = new \App\Models\Oai_pmh\OaiPmhModel();
         $data['seals'] = $Seals->findAll();
         $data['totalRepositorios'] = $Oai->totalRepositoriosAvaliados();
