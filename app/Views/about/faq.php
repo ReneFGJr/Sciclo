3a8bdeb9b0bbfbbdb7d5f78615856bbc236bfb50
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-05-06 12:31:57 -0300

    Update faq.php

--- app/Views/about/faq.php
+++ app/Views/about/faq.php
@@ -16,7 +16,7 @@
                     </h2>
                     <div id="collapse<?= $i ?>" class="accordion-collapse collapse <?= $i === 0 ? 'show' : '' ?>" aria-labelledby="heading<?= $i ?>" data-bs-parent="#faqAccordion">
                         <div class="accordion-body">
-                            <?= str_replace("\n", "<br><br>", esc($faq['answer'])) ?>
+                            <?= str_replace("\n", "<br>", esc($faq['answer'])) ?>
                         </div>
                     </div>
                 </div>
