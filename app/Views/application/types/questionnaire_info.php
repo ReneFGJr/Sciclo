b7e35092fa3a32360aa393c4fbf4f79e7716915f
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-05-06 09:27:01 -0300

    Update questionnaire_info.php

--- app/Views/application/types/questionnaire_info.php
+++ app/Views/application/types/questionnaire_info.php
@@ -1,6 +1,6 @@
 <div class="card mb-3">
     <div class="card-body">
-        <h5 class="card-title">Questão #<?= esc($q['id']) ?></h5>
+        <h5 class="card-title">Critério <?= esc($q['id']) ?></h5>
         <p class="card-text mb-2"><?= esc($q['descricao']) ?></p>
         <?php if (!empty($q['alternativas'])): ?>
             <?php foreach ($q['alternativas'] as $alt): ?>
